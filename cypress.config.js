4fc6a485c7513dd667e0934cde47f50297de28d3
NTN-code <92088758+AntonTkacouITechArt@users.noreply.github.com>
2022-11-22 19:13:51 +0300

    fix ci 9

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 const fs = require('fs')
 
 module.exports = defineConfig({
+  projectId: "x1xdqk",
+  
   e2e: {
     baseUrl: "https://www.apple.com",
     experimentalModifyObstructiveThirdPartyCode: true,
